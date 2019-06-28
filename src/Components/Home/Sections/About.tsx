87c251ad0975affb9a86ff4abe5197caa99a01e5
Christian Barra <barrachri@gmail.com>
2019-06-28 11:48:50 +0200

    Remove pizza

--- src/Components/Home/Sections/About.tsx
+++ src/Components/Home/Sections/About.tsx
@@ -10,7 +10,7 @@ export default class About extends React.Component {
                     <h1>Python Pizza</h1>
                     <p><b>Python Pizza</b> is a micro conference organized by the Python Berlin Community.</p>
                     <p>
-                        We bring the Python Community together for some amazing original Pizza™ and Python Talks.</p>
+                        We bring the Python Community together for some amazing food and Python Talks.</p>
                     <p>We believe in and encourage practical sessions, in which developers share their experience and lessons from real-world projects, each talk is 10 mins long.</p>
                     <p>Thanks to the Python Pizza (Naples!) that started this format!</p>
                     <br></br>
