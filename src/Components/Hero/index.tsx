cde67d42ce7af6152219bddcaf97f57f8249aeea
Theo Costea <theo.costea@gmail.com>
2020-05-18 15:15:03 +0200

    Add Berlin to header to prevent confusion

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -9,7 +9,7 @@ export default class Hero extends React.Component {
         return (
             <div className="hero">
                 <div className="hero--info">
-                    <h1>Python Pizza</h1>
+                    <h1>Python Pizza Berlin</h1>
                     <h2>August 2020 - TBA</h2>
                     {/* <h2>
                         Berlin @ Wooga <br />
